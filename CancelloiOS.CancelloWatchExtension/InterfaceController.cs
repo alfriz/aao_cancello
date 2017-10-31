203a33d16496ef328020f520ffcc8a03cc19f3b0
Costanza <costanza.minarelli@psmobile.it>
2017-10-31 23:12:41 +0100

    Prima versione watch-app

---
+++ CancelloiOS.CancelloWatchExtension/InterfaceController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using WatchKit;
+using Foundation;
+using System.Threading.Tasks;
+using SocketLite.Services;
+using System.Text;
+using System.Collections.Generic;
+
+namespace CancelloiOS.CancelloWatchExtension
+{
+    public partial class InterfaceController : WKInterfaceController
+    {
+        protected InterfaceController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        int i = 0;
+		partial void ButtonTapped()
+		{
+            i++;
+            //Devo inviare ogni volta un messaggio diverso se no fa una sorta di distinct e non lo rimanda
+            WCSessionManager.SharedManager.UpdateApplicationContext(new Dictionary<string, object>() { { "PippaCancello", i.ToString() } });
+		}
+		
+        public override void Awake(NSObject context)
+        {
+            base.Awake(context);
+
+            // Configure interface objects here.
+            Console.WriteLine("{0} awake with context", this);
+        }
+
+        public override void WillActivate()
+        {
+            // This method is called when the watch view controller is about to be visible to the user.
+            Console.WriteLine("{0} will activate", this);
+        }
+
+        public override void DidDeactivate()
+        {
+            // This method is called when the watch view controller is no longer visible to the user.
+            Console.WriteLine("{0} did deactivate", this);
+        }
+    }
+}
---
+++ CancelloiOS/ViewController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CancelloiOS.CancelloWatchExtension;
+using SocketLite.Services;
+using UIKit;
+using WatchConnectivity;
+
+namespace CancelloiOS
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            WCSessionManager.SharedManager.ApplicationContextUpdated += DidReceiveApplicationContext;
+        }
+
+        public override void ViewDidUnload()
+        {
+            base.ViewDidUnload();
+            WCSessionManager.SharedManager.ApplicationContextUpdated -= DidReceiveApplicationContext;
+        }
+
+        private void DidReceiveApplicationContext(WCSession session, Dictionary<string, object> applicationContext)
+        {
+            var message = (string)applicationContext["PippaCancello"];
+            if(message != null)
+                Task.Run (async () => await OpenCancello());
+        }
+
+        async Task OpenCancello()
+        {
+            try
+            {
+                var tcpClient = new TcpSocketClient();
+                await tcpClient.ConnectAsync("192.168.0.170", "8090");
+
+                var command = "/OUT1";
+
+                var bytes = Encoding.UTF8.GetBytes(command);
+                await tcpClient.WriteStream.WriteAsync(bytes, 0, bytes.Length);
+                tcpClient.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
